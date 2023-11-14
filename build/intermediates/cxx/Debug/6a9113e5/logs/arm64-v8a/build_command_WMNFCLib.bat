e22d05cd008df7f871a38aa9dceded223ebbcbda
AhmedMLasheen <85111481+AhmedMLasheen@users.noreply.github.com>
2023-11-14 12:00:25 +0200

    add build

---
+++ build/intermediates/cxx/Debug/6a9113e5/logs/arm64-v8a/build_command_WMNFCLib.bat
@@ -0,0 +1,5 @@
+@echo off
+"D:\\Andorid\\SDK\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "D:\\T2020G\\Java\\GTNFCTestTool\\gtnfctesttool\\WMNFCLib\\.cxx\\Debug\\6a9113e5\\arm64-v8a" ^
+  WMNFCLib
---
+++ build/intermediates/cxx/Debug/6a9113e5/logs/x86/build_command_WMNFCLib.bat
@@ -0,0 +1,5 @@
+@echo off
+"D:\\Andorid\\SDK\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "D:\\T2020G\\Java\\GTNFCTestTool\\gtnfctesttool\\WMNFCLib\\.cxx\\Debug\\6a9113e5\\x86" ^
+  WMNFCLib
